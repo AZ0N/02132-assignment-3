a992a8c8f62b4afc5ea769b8a490976171eb6915
DeMoManique <frederik.hvarregaard.andersen@gmail.com>
2023-11-24 18:20:28 +0100

    FSMD's withins FSMD's
    fsmDEEZ NUTS

--- src/main/scala/Accelerator.scala
+++ src/main/scala/Accelerator.scala
@@ -14,10 +14,12 @@ class Accelerator extends Module {
   })
 
   // State enum and register
-  val init  :: done :: borderTop :: borderBottom :: borderLeft :: borderRight :: forLoop :: blackCenter :: blackTop :: blackBottom :: blackLeft :: blackRight :: firstRow :: lastRow :: rowBlack :: rowWhite :: rowFiveInit :: rowFourInit :: rowFiveBlack :: rowFiveWhite :: rowFourWhite :: rowFourBlack:: Nil = Enum(22)
+  val init  :: done :: border :: specialRow :: borderTop :: borderBottom :: borderLeft :: borderRight :: forLoop :: blackCross :: firstRow :: lastRow :: rowBlack :: rowWhite :: rowFiveInit :: rowFourInit :: rowFiveBlack :: rowFiveWhite :: rowFourWhite :: rowFourBlack:: Nil = Enum(20)
   val rowOne :: rowTwo :: rowThree :: rowFour :: rowFive :: Nil = Enum(5)
   val white :: black :: written :: Nil = Enum(3)
+  val left :: right :: up :: down :: center :: rowFourFive :: Nil = Enum(6)
   val stateReg = RegInit(init)
+  val innerState = RegInit(up)
 
   // Support registers
   val x = RegInit(0.U(32.W))
@@ -43,7 +45,7 @@ class Accelerator extends Module {
       writeBlack(0.U,0.U)
       
       when (io.start) {
-        stateReg := borderTop
+        stateReg := border
       } .otherwise {
         stateReg := init
       }
@@ -55,116 +57,23 @@ class Accelerator extends Module {
     is(forLoop) {
       io.address := 20.U * y + x
       when (io.dataRead === 0.U){
-        stateReg := blackCenter
+        innerState := center
+        stateReg := blackCross
       } .otherwise {
+        innerState := left
         stateReg := done
       }
     }
-    is(blackCenter){
-      writeBlack(x, y)
-      when (x > 1.U){ //if we can go left
-        stateReg := blackLeft
-      } .otherwise{
-        when (x < 18.U){ //else if we can go right
-          stateReg := blackRight
-        } . otherwise{
-          when (y > 1.U){ //else if we can go up
-            stateReg := blackTop
-          } .otherwise{
-            when (y < 18.U){ //else if we can go down
-              stateReg := blackBottom
-            } .otherwise{
-              loop()
-            }
-          }
-        }
-      }
+    is(blackCross) {
+      blackFSMD()
     }
-    is(blackLeft){
-      writeBlack(x - 1.U, y)
-      when (x < 18.U){ //if we can go right
-        stateReg := blackRight
-      } .otherwise{
-        when (y > 1.U){ //else if we can go up
-          stateReg := blackTop
-        } .otherwise{
-          when (y < 18.U){ //else if we can go down
-            stateReg := blackBottom
-          } .otherwise{
-            loop()
-          }
-        }
-      }
+    is(border){
+      borderFSMD()
     }
-    is(blackRight){
-      writeBlack(x + 1.U, y)
-        when (y > 1.U){ //else if we can go up
-          stateReg := blackTop
-        } .otherwise{
-          when (y < 18.U){ //else if we can go down
-            stateReg := blackBottom
-          } .otherwise{
-            loop()
-          }
-        }
+    is(specialRow){
+      specialRowFSMD()
     }
-    is(blackTop) {
-      writeBlack(x,y - 1.U)
-      when ( y < 18.U){
-        stateReg := blackBottom
-      } .otherwise {
-        loop()
-      }
-    }
-    is(blackBottom) {
-      writeBlack(x,y + 1.U)
-      loop()
-    }
-    is(borderTop) {
-      writeBlack(x + 1.U,y)
-      when (x === 18.U) {
-        stateReg := borderBottom
-        x := 0.U
-        y := 19.U
-      } .otherwise {
-        x := x + 1.U
-        stateReg := borderTop
-      }
-    }
-    is(borderBottom) {      
-      writeBlack(x + 1.U,y)
-      when (x === 18.U) {
-        x := 0.U
-        y := 0.U
-        stateReg := borderLeft
-      } .otherwise {
-        x := x + 1.U
-        stateReg := borderBottom
-      }
-    }
-    is(borderLeft) {
-      writeBlack(x,y + 1.U)
-      when (y === 18.U) {
-        x := 19.U
-        y := 0.U
-        stateReg := borderRight
-      } .otherwise {
-        y := y + 1.U
-        stateReg := borderLeft
-      }
-    }
-    is(borderRight) {
-      writeBlack(x,y + 1.U)
-      when (y === 17.U) {
-        x := 3.U
-        y := 1.U
-        rowType := rowFive
-        stateReg := firstRow
-      } .otherwise {
-        y := y + 1.U
-        stateReg := borderRight
-      }
-    }
+    //skal rykkes ind i special row på en eller anden måde
     is(firstRow){
       io.address := 20.U * y + x + 400.U
       when(io.dataRead === 0.U){
@@ -181,59 +90,31 @@ class Accelerator extends Module {
         stateReg := rowWhite
       }
     }
-    is(rowFiveInit){
-      io.address := 20.U * y + x + 400.U
-      when(io.dataRead === 0.U){
-        stateReg := rowFiveBlack
-      } .otherwise{
-        stateReg := rowFiveWhite
-      }
-    }
-    is(rowFourInit){
-      io.address := 20.U * y + x + 400.U
-      when(io.dataRead === 0.U){
-        stateReg := rowFourBlack
-      } .otherwise{
-        stateReg := rowFourWhite
-      }
-    }
     is(rowBlack){
       writeBlack(x,y)
       when(x >= 16.U){
           when(rowType === rowFive){
           x := 1.U
-          stateReg := rowFiveInit
+          stateReg := specialRow
+          innerState := rowFourFive
+          rowType := rowFive
           }.otherwise{
             x:=18.U
-            stateReg := rowFourInit
+            stateReg := specialRow
+            innerState := rowFourFive
+            rowType := rowFour
           }
       }.otherwise{
         x := x + 5.U
         when(rowType === rowFive){
             stateReg := firstRow
           }.otherwise{
-            stateReg := lastRow
+            stateReg := firstRow
           }
       }
       
     }
     is(rowWhite){
-      stateReg := done
-    }
-    is(rowFiveBlack){
-      writeBlack(x,y)
-      x := 5.U
-      stateReg := forLoop
-    }
-    is(rowFiveWhite){
-      stateReg := done
-    }
-    is(rowFourBlack){
-      writeBlack(x,y)
-      x := 4.U
-      stateReg := forLoop
-    }
-    is(rowFourWhite){
       stateReg := done
     }
   }
@@ -281,12 +162,14 @@ class Accelerator extends Module {
           x := 3.U
         } .otherwise {
           when(rowType === rowTwo){
-            stateReg := rowFourInit
+            stateReg := specialRow
+            innerState := rowFourFive
             rowType := rowFour
             x := 18.U
           } .otherwise {
             when(rowType === rowThree){
-              stateReg := rowFiveInit
+              stateReg := specialRow
+              innerState := rowFourFive
               rowType := rowFive
               x := 1.U
             } .otherwise {
@@ -307,12 +190,168 @@ class Accelerator extends Module {
       }
     }   
   }
-
   def updateRegisters(): Unit = {
     prevLine := thisLine
     thisLine := nextLine
     for (i <- 0 until 18) {
       nextLine(i) := 0.U
+    }
+  }
+
+  //Seperate FSMD is a read pixel is black
+  def blackFSMD(): Unit = {
+    switch(innerState){
+        is(center){
+          writeBlack(x, y)
+          when (x > 1.U){ //if we can go left
+            innerState := left
+          } .otherwise{
+            when (x < 18.U){ //else if we can go right
+              innerState := right
+            } . otherwise{
+              when (y > 1.U){ //else if we can go up
+                innerState := up
+              } .otherwise{
+                when (y < 18.U){ //else if we can go down
+                  innerState := down
+                } .otherwise{
+                  loop()
+                }
+              }
+            }
+          }
+        }
+        is(left){
+          writeBlack(x - 1.U, y)
+          when (x < 18.U){ //if we can go right
+            innerState := right
+          } .otherwise{
+            when (y > 1.U){ //else if we can go up
+              innerState := up
+            } .otherwise{
+              when (y < 18.U){ //else if we can go down
+                innerState := down
+              } .otherwise{
+                innerState := center
+                loop()
+              }
+            }
+          }
+        }
+        is(right){
+          writeBlack(x + 1.U, y)
+            when (y > 1.U){ //else if we can go up
+              innerState := up
+            } .otherwise{
+              when (y < 18.U){ //else if we can go down
+                innerState := down
+              } .otherwise{
+                innerState := center
+                loop()
+              }
+            }
+        }
+        is(up) {
+          writeBlack(x,y - 1.U)
+          when ( y < 18.U){
+            innerState := down
+          } .otherwise {
+            innerState := center
+            loop()
+          }
+        }
+        is(down) {
+          writeBlack(x,y + 1.U)
+          innerState := center
+          loop()
+        }
+      }
+  }
+  //Seperate FSMD is a read pixel is white
+  def whiteFSMD(): Unit = {
+    
+  }
+
+  def specialRowFSMD(): Unit = {
+    switch(innerState){
+      is(rowFourFive){
+        when(rowType === rowFive){
+          io.address := 20.U * y + 1.U
+          when(io.dataRead === 0.U){
+            innerState := black
+          } .otherwise{
+            innerState := white
+          }
+        }.otherwise{
+          io.address := 20.U * y + 18.U
+          when(io.dataRead === 0.U){
+            innerState := black
+          } .otherwise{
+            innerState := white
+          }
+        }
+      }
+      is(black){
+        when(rowType === rowFive){
+          writeBlack(1.U,y)
+          x := 5.U
+          stateReg := forLoop
+        }.otherwise{
+          writeBlack(18.U,y)
+          x := 4.U
+          stateReg := forLoop
+        }
+      }
+      is(white){
+
+      }
+    }
+  }
+
+  //Seperate FSMD for drawing border
+  def borderFSMD(): Unit = {
+    switch(innerState){
+      is(up){
+        writeBlack(x + 1.U,y)
+        when (x === 18.U) {
+          innerState := down
+          x := 0.U
+          y := 19.U
+        } .otherwise {
+          x := x + 1.U
+        }
+      }
+      is(down){
+        writeBlack(x + 1.U,y)
+        when (x === 18.U) {
+          x := 0.U
+          y := 0.U
+          innerState := left
+        } .otherwise {
+          x := x + 1.U
+        }
+      }
+      is(left){
+        writeBlack(x,y + 1.U)
+        when (y === 18.U) {
+          x := 19.U
+          y := 0.U
+          innerState := right
+        } .otherwise {
+          y := y + 1.U
+        }
+      }
+      is(right){
+        writeBlack(x,y + 1.U)
+        when (y === 17.U) {
+          x := 3.U
+          y := 1.U
+          rowType := rowFive
+          stateReg := firstRow
+        } .otherwise {
+          y := y + 1.U
+        }
+      }
     }
   }
 }
